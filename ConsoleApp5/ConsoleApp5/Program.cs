30fcde7441dd62e1f6e3163a02614330f9b18b9c
oleshaaa69 <106986336+oleshaaa69@users.noreply.github.com>
2022-06-07 12:16:25 +0700

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 1; i < n; i++)
+{
+    int lastN = n % 10;
+    n = n/10;
+    if (lastN > 5)
+    {
+        
+        sum = sum + lastN;
+    }
+   
+}
+Console.WriteLine(sum);
