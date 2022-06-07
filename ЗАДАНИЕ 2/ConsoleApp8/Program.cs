89c45b2b43e85e437c9ca67f7701f7bdc4b2cb91
oleshaaa69 <106986336+oleshaaa69@users.noreply.github.com>
2022-06-07 12:17:55 +0700

    Add files via upload

---
+++ ЗАДАНИЕ 2/ConsoleApp8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+    class Program
+    {
+        static void Main()
+        {
+            DateTime dt;
+            int a = int.Parse(Console.ReadLine()); //day
+            int b = int.Parse(Console.ReadLine()); //month
+            int c = int.Parse(Console.ReadLine()); //year
+            string date = $"{a}, {b}, {c}";
+
+            JulianCalendar calendar = new JulianCalendar();
+            if (DateTime.TryParse(date, out dt))
+            {
+                if (b != 2)
+                {
+                    a = a + 13;
+                }
+                if (a > 30)
+                {
+                    int temp = a - 30;
+                    a = temp;
+                    b += 1;
+                }
+                if (b > 12)
+                {
+                    int temp = b - 12;
+                    b = temp;
+                    c += 1;
+                }
+                Console.WriteLine($"{ a} {b} {c}");
+            }
+            else
+            {
+                Console.WriteLine("Неверная дата ");
+            }
+        }
+    }
+
